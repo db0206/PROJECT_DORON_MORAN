fc31b72511cb8f7c3399cbab0df61fdf32eb86ef
DORON <doronbrgr@yahoo.com>
2024-01-26 18:21:17 +0200

    NOTES

---
+++ src/CLS/ConnectionPool.java
@@ -0,0 +1,69 @@
+package CLS;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Stack;
+
+public class ConnectionPool {
+
+    private static final int NUMBER_OF_CONNECTIONS = 10;
+    public static ConnectionPool instance = null;
+    private final Stack<Connection> connections = new Stack<>();
+
+    private ConnectionPool() throws SQLException {
+        System.out.println("We create new connection pool instance");
+        openAllConnections();
+    }
+
+    private void openAllConnections() throws SQLException {
+        //create connections according to Number of Connections
+        for (int counter = 0; counter < NUMBER_OF_CONNECTIONS; counter++) {
+            //create connection
+            Connection connection = DriverManager.getConnection(DBManager.URL, DBManager.SQL_USER, DBManager.SQL_PASS);
+            //push the new connection into stack "connections".
+            connections.push(connection);
+        }
+    }
+
+    private void closeAllConnections() throws InterruptedException {
+        synchronized (connections) {
+            while (connections.size() < NUMBER_OF_CONNECTIONS) {
+                connections.wait();
+            }
+            connections.removeAllElements();
+        }
+    }
+
+    public static ConnectionPool getInstance(){
+        if (instance==null){
+            synchronized (ConnectionPool.class){
+                if(instance==null){
+                    try {
+                        instance = new ConnectionPool();
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());;
+                    }
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws InterruptedException {
+        synchronized (connections){
+            if (connections.isEmpty()){
+                connections.wait();
+            }
+            return connections.pop();
+        }
+    }
+
+    public void returnConnection(Connection connection){
+        synchronized (connections){
+            connections.push(connection);
+            //notify that we got back a connection for the user
+            connections.notify();
+        }
+    }
+}
---
+++ src/CLS/DBManager.java
@@ -0,0 +1,12 @@
+package CLS;
+
+public class DBManager {
+    // connection to mysql
+
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String SQL_USER = "root";
+    public static final String SQL_PASS = "12345678";
+
+    //DataBase
+    public static final String DB = "project_doron_moran";
+}
