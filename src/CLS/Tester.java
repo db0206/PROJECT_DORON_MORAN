639ca5c955821bb0c85ac24bbf1d4f66f2742dad
Moran <morandce@gmail.com>
2024-01-29 16:28:18 +0200

    Tester try

--- src/CLS/Tester.java
+++ src/CLS/Tester.java
@@ -1,7 +1,9 @@
 package CLS;
 
 import Beans.Category;
+import Beans.Company;
 import Beans.Coupon;
+import Beans.Customer;
 
 import java.time.LocalDate;
 
@@ -10,5 +12,34 @@ public class Tester {
     public static void main(String[] args) {
 
         Coupon coupon = new Coupon(1,2, ,"camera","buy the camera fifty percent off", LocalDate.now(), LocalDate.now(),10,20.99,"this image" );
+
+
+        System.out.println("============MORAN-TRY============");
+
+        Company company1 = new Company(01, "MDK", "mdk@gmail.com", "MDK1234");
+        System.out.println(company1);
+
+        Coupon coupon1 = new Coupon(01,1, Category.NEW_GEAR,"SONY A7SIII",
+                "Mirrorless camera",
+                LocalDate.of(2024,1,1),
+                LocalDate.of(2024,1,31),
+                10,16_000.0,null);
+
+        company1.addCoupon(coupon1);
+
+        System.out.println("=================================");
+
+        System.out.println(company1);
+
+        Customer customer1 = new Customer(01,"Moran", "Ariel",
+                "morandce@gmail.com", "1234" );
+        System.out.println("=================================");
+        customer1.addCoupon(coupon1);
+
+        System.out.println(customer1);
+
+
+
+
     }
 }
